adcc937d68af429c5234ffcd4f3627659435847d
Davide Zanella <d.zanella@codermine.com>
2025-05-08 00:04:02 +0200

    persistReducer implemented

--- crwn-store/src/store/store.js
+++ crwn-store/src/store/store.js
@@ -1,9 +1,23 @@
 import { compose, legacy_createStore as createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-// root-reducer
-const middlewares = [logger];
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    blacklist: ['user']
+};
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
